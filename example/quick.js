8e479aeac81de98892910c8d32b029c28e6f0fbf
James Halliday <mail@substack.net>
2015-08-24 13:38:39 -0500

    quick example

---
+++ example/quick.js
@@ -0,0 +1,11 @@
+var KP = require('../')
+var DHT = require('bittorrent-dht')
+
+var dht = new DHT({ verify: KP.verify })
+var kp = KP()
+
+var value = 'beep boop'
+dht.put(kp.store(value), function (errors, hash) {
+  if (errors.length) errors.forEach(console.log)
+  else console.log(kp.id)
+})
